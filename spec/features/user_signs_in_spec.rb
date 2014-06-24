e4177a62c6349f05caecb44a013d781acd0cabd1
Caleb Thill <calebnthill@gmail.com>
2014-06-24 14:50:58 -0400

    added 3 tests... oops

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+feature 'User can sign in or sign up' do
+
+  scenario "User can sign up sucessfully" do
+    #user = FactoryGirl.new(:user)
+
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'Email', with: 'user@gmail.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign Up'
+
+    expect(page).to have_content('Thanks for signing up!')
+    expect(page).to have_content('Sign Out')
+  end
+
+
+
+
+  scenario "User doesn't provide a username or password when they sign up" do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign Up'
+
+    expect(page).to have_content('Email can\'t be blank')
+  end
+
+  scenario "User can sign in when they provide their input" do
+
+    user1 = User.create!(first_name: "bob", last_name: "love", email: "calebnthill@ygmail.com",
+                         password: "password", password_confirmation: "password")
+    visit root_path
+    click_link "Sign In"
+    fill_in "Email", with: user1.email
+    fill_in "Password", with: user1.password
+
+    click_button "Sign in"
+
+    expect(page).to have_content('Signed in successfully.')
+  end
+  scenario "User is presented with an error with invalid input"
+end
