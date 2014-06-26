2573f9359ec4d76068d13606291dd9eca97a21f0
David Guerette <david.guerette@gmail.com>
2014-06-26 17:41:51 -0400

    Added CarrierWave

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  mount_uploader :profile_photo, ProfilePhotoUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,5 +11,4 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  # mount_uploader :profile_photo, ProfilePhotoUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'shops#index'
 
   devise_for :users
   resources :shops, only: [ :index, :new, :create, :show, :edit, :update, :destroy] do
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   #You can have the root of your site routed with "root"
-  root 'shops#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
