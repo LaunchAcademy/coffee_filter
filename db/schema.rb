429f8ea9e17c680a86d8615995dd6a126a384e32
David Guerette <david.guerette@gmail.com>
2014-06-26 14:05:02 -0400

    Fixed reviews strong params

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625213443) do
+ActiveRecord::Schema.define(version: 20140626180003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20140625213443) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "shop_id"
-    t.integer  "user_id"
+    t.integer  "user_id",    null: false
   end
 
   create_table "shops", force: true do |t|
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20140625213443) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
