275145fac8f2614f6021aecf8451d805acf405eb
Caleb Thill <calebnthill@gmail.com>
2014-07-01 13:30:09 -0400

    Fixing the admin

--- spec/features/admin_has_full_administrative_control_spec.rb
+++ spec/features/admin_has_full_administrative_control_spec.rb
@@ -7,7 +7,7 @@ feature 'administrator has full CRUD control' do
     shop_count = Shop.count
 
     log_in(admin)
-    visit admin_shops_path
+    visit shops_path
     click_on "Delete"
 
     expect(page).to have_content("Coffee shop deleted!")
@@ -21,32 +21,12 @@ feature 'administrator has full CRUD control' do
     shop_count = Shop.count
 
     log_in(admin)
-    visit admin_shops_path
+    visit shops_path
     click_on "Edit"
     fill_in 'Coffee shop name', with: "1369"
     click_on 'Update shop'
 
     expect(page).to have_content("1369")
     expect(Shop.count).to eq(shop_count)
-  end
-
-  scenario 'admin is able to create a shop' do
-    admin = FactoryGirl.create(:user, role: 'admin')
-    shop = FactoryGirl.build(:shop)
-    shop_count = Shop.count
-
-    log_in(admin)
-    visit admin_shops_path
-    click_on "Add a coffee shop"
-
-    fill_in "Coffee shop name", with: shop.name
-    fill_in "Address", with: shop.address
-    fill_in "Description", with: shop.description
-    click_on "Add Shop"
-
-    expect(page).to have_content(shop.name)
-    expect(page).to have_content(shop.address)
-    expect(page).to have_content(shop.description)
-    expect(Shop.count).to eq(shop_count + 1)
   end
 end
