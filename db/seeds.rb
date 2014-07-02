0858e52d399d545ca775938f901139afbc479298
Caleb Thill <calebnthill@gmail.com>
2014-07-02 11:58:11 -0400

    added a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create_table "shops", force: true do |t|
+#     t.string   "name",        null: false
+#     t.string   "address",     null: false
+#     t.text     "description", null: false
+#   end
+# end
+
+Shop.create(name: 'Wierd puppy', address: '250 Newbury St, Boston, MA 02116',
+           description: 'Great fun hangout place for an early morning study session!')
+
+Shop.create(name: 'Thinking Cup', address: '165 Tremont St, Boston, MA 02111',
+           description: 'Amazing iced coffee and great baked goods. No WIFI though!')
+
+Shop.create(name: 'Flat Black Coffee', address: '50 Broad St, Boston, MA 02109',
+           description: 'Fun little coffee shop!')
+
+Shop.create(name: 'Render Coffee', address: '563 Columbus Ave, Boston, MA 02118',
+           description: 'Great coffe and sandwiches! In the back of the shop, there is a room that has a domed glass roof! super cool!')
+
+Shop.create(name: "Polcari's coffee", address: '105 Salem St, Boston, MA 02113',
+           description: 'Great fun hangout place for an early morning study session!')
+
+Shop.create(name: 'Capitol Coffee House', address: '122 Bowdoin St, Boston, MA 02108',
+           description: 'Super cute coffee shop! The coffee is great as well as the food!')
+
+Shop.create(name: 'Caffe Vittoria', address: '290 Hanover St, Boston, MA 02113',
+           description: 'Amazing little coffee shop in the heart of the north end!')
+
+Shop.create(name: 'Pavement Coffeehouse', address: '1096 Boylston St, Boston, MA 02215',
+           description: 'Rustic, cozy joint with tall ceilings & leather banquettes serving artisan coffee, teas & bagels.')
+
+Shop.create(name: 'Caffe Paradiso', address: '255 Hanover St, Boston, MA 02113',
+           description: 'Locals head to this traditional Italian coffeehouse for espresso, gelato, pastries & socializing.')
+
+Shop.create(name: 'Equal Exchange Cafe', address: '226 Causeway St, Boston, MA 02114',
+           description: 'Cozy coffee shop bringing together drinks sourced from a local co-op & a veggie-heavy food menu.')
+
+Shop.create(name: '1369 Coffeehouse', address: '757 Massachusetts Ave, Cambridge, MA 02139',
+           description: 'Top-notch coffee & sandwiches draw local creative types to this coffeeshop featuring a gallery!!')
+
+Shop.create(name: 'Crema Cafe', address: '27 Brattle St, Cambridge, MA 02138',
+           description: 'Super cool coffee shop in the heart of Harvard square! NO WIFI!)
+
+
+
+
+
+
+
+
+
+
+
