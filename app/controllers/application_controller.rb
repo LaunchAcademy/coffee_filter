8c4fa92e18ab27a9c6a8e3a1e9acb5ccba9f8e85
Caleb Thill <calebnthill@gmail.com>
2014-07-03 10:42:47 -0400

    fixed failing test on bootstrap branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,11 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
+    shops_path
+  end
+
   protected
 
   def configure_permitted_parameters
